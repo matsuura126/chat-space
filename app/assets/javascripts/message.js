0c040349c31d0f55aebb664489f0884ab31f3be7
ma2-ra <matsu.bo.9ri@gmail.com>
2020-06-28 11:28:25 +0900

    add latest message to message list

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,35 +2,35 @@ $(function(){
   function buildHTML(message){
     // 「もしメッセージに画像が含まれていたら」という条件式
     if (message.image) {
-      var html = `<div class="message-items">` +
-        `<div class="message-items__top">` +
-        `<p class="message-items__top__name">` +
-        message.user_name +
-        `</p>` +
-        `<p class="message-items__top__date">` +
-        message.created_at +
-        `</p>` +
-        `</div>` +
-        `<p class="message-items__message">` +
-        message.content +
-        `<img class="messageJ-items__image" src="` + message.image + `" alt="Amaebi computer">` +
-        `</p>` +
-        `</div>`
+      var html = `<div class="message-items">
+        <div class="message-items__top" data-message-id="${message.id}>
+        <p class="message-items__top__name">
+        ${message.user_name}
+        </p>
+        <p class="message-items__top__date">
+        ${message.created_at}
+        </p>
+        </div>
+        <p class="message-items__message">
+        ${message.content}
+        <img class="messageJ-items__image" src="${message.image}" alt="Amaebi computer">
+        </p>
+        </div>`
       return html;
     } else {
-      var html = `<div class="message-items">` +
-        `<div class="message-items__top">` +
-        `<p class="message-items__top__name">` +
-        message.user_name +
-        `</p>` +
-        `<p class="message-items__top__date">` +
-        message.created_at +
-        `</p>` +
-        `</div>` +
-        `<p class="message-items__message">`+
-        message.content +
-        `</p>` +
-        `</div>` 
+      var html = `<div class="message-items">
+        <div class="message-items__top" data-message-id="${message.id}>
+        <p class="message-items__top__name">
+        ${message.user_name}
+        </p>
+        <p class="message-items__top__date">
+        ${message.created_at}
+        </p>
+        </div>
+        <p class="message-items__message">
+        ${message.content}
+        </p>
+        </div>` 
       return html;
     };
   }
@@ -58,4 +58,24 @@ $(function(){
       $('.submit-btn').prop('disabled', false);
     })
   })
+
+  var reloadMessages = function() {
+    var last_message_id = $('.message-items__top:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      var insertHTML = '';
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+      $('.messages').append(insertHTML);
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
 });
